e306630be727ab7374f11f98525a1906fb01e6d5
pavankalyan2001 <71204811+pavankalyan2001@users.noreply.github.com>
2025-03-09 12:51:05 +0530

    Add files via upload

---
+++ EduConApplication.java
@@ -0,0 +1,13 @@
+package com.edu.con;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduConApplication.class, args);
+	}
+
+}
